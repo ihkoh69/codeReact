14ae9599303bdf44ae87109cef61413f4ffacbc2
Steve Inhun Ko <yoimono@naver.com>
2025-07-22 19:28:42 +0900

    '.'

---
+++ mallApi/src/test/java/kr/bulog/mallapi/MallApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.bulog.mallapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
